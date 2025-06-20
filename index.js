4e16f56f984f24dba76950c21ace465a70962c74
satyam1708 <pandeysatyam1708@gmail.com>
2025-06-20 10:22:59 +0530

    .env update and index.js app port update

--- index.js
+++ index.js
@@ -574,4 +574,9 @@ app.get("/privacy-policy", (req, res) => {
 });
 
 // Start Server
-app.listen(5000, () => logger.info("Auth server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// app.listen(5000, () => logger.info("Auth server running on http://localhost:5000"));
