b2cfc649e3a650723456aba918d948548151b61e
satyam1708 <pandeysatyam1708@gmail.com>
2025-06-28 11:34:54 +0530

    https://propai.cognitiev.com/

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const allowedOrigins = [
   "http://localhost:8080",
   "https://voice.cognitiev.com",
   "https://youragency2.netlify.app",
-  "https://propai.cognitiev.com/"
+  "https://propai.cognitiev.com"
 ];
 
 app.use(
